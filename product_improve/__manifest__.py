223bb040b1156009c12f2f4ceed7f75feab7d5cd
odoo-flario-uae <117816078+odoo-flario-uae@users.noreply.github.com>
2022-11-23 19:37:25 +0300

    Add files via upload

---
+++ product_improve/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Product: Improvments",
+    "summary": """Product model improvments""",
+    "author": "EVM",
+    "category": "Uncategorized",
+    "version": "16.0.0.0.1",
+    "depends": [
+        "product",
+    ],
+    "data": [
+        "views/product_views.xml",
+    ],
+}
---
+++ product_improve/models/product_template.py
@@ -0,0 +1,31 @@
+from odoo import models, fields, api
+
+class ProductTemplate(models.Model):
+    _inherit = "product.template"
+
+    #### BEGIN Могилевец 23.11.2022  ####
+    pricelist_price = fields.Float(
+        'Pricelist Price', compute='_compute_template_pricelist_price', digits='Product Price')
+
+    def _compute_template_pricelist_price(self):
+        for template in self:
+            pricelist_items = template.env['product.pricelist.item'].search(
+                ["&","&",
+                 "|", ["product_tmpl_id", "=", template.id], ["product_id", "in", template.product_variant_ids.ids],
+                 "|", ["company_id", "=", self.env.company.id], ["company_id", "=", False],
+                 ["currency_id", "=", template.currency_id.id]
+                 ]
+            )
+            if len(pricelist_items) == 1:
+                template.pricelist_price = template.with_context(pricelist=pricelist_items[0].pricelist_id.id).price
+            else:
+                template.pricelist_price = 0.0
+
+    # Изменение вычисления поля 'template.currency_id'
+    @api.depends('company_id')
+    def _compute_currency_id(self):
+        main_company = self.env['res.company']._get_main_company()
+        for template in self:
+            # template.currency_id = template.company_id.sudo().currency_id.id or main_company.currency_id.id
+            template.currency_id = template.company_id.sudo().currency_id.id or self.env.company.currency_id.id or main_company.currency_id.id
+    #### END Могилевец 23.11.2022  ####
