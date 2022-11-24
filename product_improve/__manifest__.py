eea64fa10723395e411ce1bd42d4185a7bc03986
alnilam-odoo <w7vp)24L>
2022-11-24 15:41:38 +0300

    product

---
+++ product_improve/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Product: Improvments",
+    "summary": """Product model improvments""",
+    "author": "EVM",
+    "category": "Uncategorized",
+    "version": "16.0.0.0.1",
+    "depends": [
+        "product",
+    ],
+    "data": [
+    ],
+}
