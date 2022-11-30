a0078d4a1e5f97b4b9e9a583a34e86ef338e5087
pandrosov <pandrosov@list.ru>
2022-11-30 18:44:17 +0300

    [ALTER FIELDS] Change fields and view

--- product_improve/models/product_template.py
+++ product_improve/models/product_template.py
@@ -2,8 +2,8 @@ from odoo import models, fields, api
 
 class ProductTemplate(models.Model):
     _inherit = "product.template"
-    directions = fields.Char(string="Directions")
-    ingredients = fields.Char(string="Ingredients")
+    extra_directions = fields.Text(string="Directions")
+    extra_ingredients = fields.Text(string="Ingredients")
 
     #### BEGIN Могилевец 23.11.2022  ####
     #pricelist_price = fields.Float('Pricelist Price', compute='_compute_template_pricelist_price', digits='Product Price')
