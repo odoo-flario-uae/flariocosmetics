88358bb92c673b0ee0a633761c1e936c4f50ab98
pandrosov <pandrosov@list.ru>
2022-12-07 15:30:06 +0300

    [UPDATE MODULE] Add related telegram fields to odoo website model

--- website_sale_improve/models/website.py
+++ website_sale_improve/models/website.py
@@ -7,3 +7,5 @@ class Website(models.Model):
     _inherit = 'website'
 
     extra_communication_method = fields.Selection(selection=[('whatsapp','WhatsApp'),('email','Email'),('by_phone','By phone')],string='Communication method')
+    telegram_token = fields.Char(string='Telegram tolen')
+    telegram_chat_id = fields.Char(string='Telegram chat ID')
