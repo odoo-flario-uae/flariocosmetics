0a6ba673c71604b8b1c262dc6569d50c0e5dcdc6
alnilam-odoo <w7vp)24L>
2022-12-08 17:48:19 +0300

    Изменение печатной формы заказа на продажу

---
+++ sale_improve/__manifest__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Sale: Improvments",
+    "summary": """Sale improvments""",
+    "author": "Flario",
+    "category": "Uncategorized",
+    "version": "16.0.0.0.1",
+    'data': [
+        'report/ir_actions_report_templates.xml',
+    ],
+    'license': 'LGPL-3',
+}
