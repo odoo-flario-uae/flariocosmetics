ca029787398a864a443e939a4110db407494ca09
alnilam-odoo <w7vp)24L>
2022-12-12 15:32:48 +0300

    Add product height, width, lenght

--- product_improve/models/product_template.py
+++ product_improve/models/product_template.py
@@ -6,67 +6,67 @@ class ProductTemplate(models.Model):
     extra_ingredients = fields.Html("Ingredients")
 
     #### BEGIN Могилевец 12.12.2022  ####
-    weight_brutto = fields.Float(string="Weight brutto", digits='Stock Weight',
-                                 compute='_compute_weight_brutto', inverse='_set_weight_brutto', store=True)
-    height = fields.Float(string="Height", compute='_compute_height', inverse='_set_height', store=True)
-    width = fields.Float(string="Width", compute='_compute_width', inverse='_set_width', store=True)
-    lenght = fields.Float(string="Lenght", compute='_compute_lenght', inverse='_set_lenght', store=True)
-    @api.depends('product_variant_ids', 'product_variant_ids.weight_brutto')
-    def _compute_weight_brutto(self):
-        unique_variants = self.filtered(lambda template: len(template.product_variant_ids) == 1)
-        for template in unique_variants:
-            template.weight_brutto = template.product_variant_ids.weight_brutto
-        for template in (self - unique_variants):
-            template.weight_brutto = 0.0
-    def _set_weight_brutto(self):
-        for template in self:
-            if len(template.product_variant_ids) == 1:
-                template.product_variant_ids.weight_brutto = template.weight_brutto
-
-    @api.depends('product_variant_ids', 'product_variant_ids.height')
-    def _compute_height(self):
-        unique_variants = self.filtered(lambda template: len(template.product_variant_ids) == 1)
-        for template in unique_variants:
-            template.height = template.product_variant_ids.height
-        for template in (self - unique_variants):
-            template.height = 0.0
-
-    def _set_height(self):
-        for template in self:
-            if len(template.product_variant_ids) == 1:
-                template.product_variant_ids.height = template.height
-
-    @api.depends('product_variant_ids', 'product_variant_ids.width')
-    def _compute_width(self):
-        unique_variants = self.filtered(lambda template: len(template.product_variant_ids) == 1)
-        for template in unique_variants:
-            template.width = template.product_variant_ids.width
-        for template in (self - unique_variants):
-            template.width = 0.0
-
-    def _set_width(self):
-        for template in self:
-            if len(template.product_variant_ids) == 1:
-                template.product_variant_ids.width = template.width
-
-    @api.depends('product_variant_ids', 'product_variant_ids.lenght')
-    def _compute_lenght(self):
-        unique_variants = self.filtered(lambda template: len(template.product_variant_ids) == 1)
-        for template in unique_variants:
-            template.lenght = template.product_variant_ids.lenght
-        for template in (self - unique_variants):
-            template.lenght = 0.0
-
-    def _set_lenght(self):
-        for template in self:
-            if len(template.product_variant_ids) == 1:
-                template.product_variant_ids.lenght = template.lenght
-
-    @api.onchange('height', 'width', 'lenght')
-    def _calc_volume(self):
-        for template in self:
-            if template.height != 0 and template.width != 0 and template.lenght != 0:
-                template.volume = (template.height * template.width * template.lenght) / 1000000000 #mm to m3
+    # weight_brutto = fields.Float(string="Weight brutto", digits='Stock Weight',
+    #                              compute='_compute_weight_brutto', inverse='_set_weight_brutto', store=True)
+    # height = fields.Float(string="Height", compute='_compute_height', inverse='_set_height', store=True)
+    # width = fields.Float(string="Width", compute='_compute_width', inverse='_set_width', store=True)
+    # lenght = fields.Float(string="Lenght", compute='_compute_lenght', inverse='_set_lenght', store=True)
+    # @api.depends('product_variant_ids', 'product_variant_ids.weight_brutto')
+    # def _compute_weight_brutto(self):
+    #     unique_variants = self.filtered(lambda template: len(template.product_variant_ids) == 1)
+    #     for template in unique_variants:
+    #         template.weight_brutto = template.product_variant_ids.weight_brutto
+    #     for template in (self - unique_variants):
+    #         template.weight_brutto = 0.0
+    # def _set_weight_brutto(self):
+    #     for template in self:
+    #         if len(template.product_variant_ids) == 1:
+    #             template.product_variant_ids.weight_brutto = template.weight_brutto
+    #
+    # @api.depends('product_variant_ids', 'product_variant_ids.height')
+    # def _compute_height(self):
+    #     unique_variants = self.filtered(lambda template: len(template.product_variant_ids) == 1)
+    #     for template in unique_variants:
+    #         template.height = template.product_variant_ids.height
+    #     for template in (self - unique_variants):
+    #         template.height = 0.0
+    #
+    # def _set_height(self):
+    #     for template in self:
+    #         if len(template.product_variant_ids) == 1:
+    #             template.product_variant_ids.height = template.height
+    #
+    # @api.depends('product_variant_ids', 'product_variant_ids.width')
+    # def _compute_width(self):
+    #     unique_variants = self.filtered(lambda template: len(template.product_variant_ids) == 1)
+    #     for template in unique_variants:
+    #         template.width = template.product_variant_ids.width
+    #     for template in (self - unique_variants):
+    #         template.width = 0.0
+    #
+    # def _set_width(self):
+    #     for template in self:
+    #         if len(template.product_variant_ids) == 1:
+    #             template.product_variant_ids.width = template.width
+    #
+    # @api.depends('product_variant_ids', 'product_variant_ids.lenght')
+    # def _compute_lenght(self):
+    #     unique_variants = self.filtered(lambda template: len(template.product_variant_ids) == 1)
+    #     for template in unique_variants:
+    #         template.lenght = template.product_variant_ids.lenght
+    #     for template in (self - unique_variants):
+    #         template.lenght = 0.0
+    #
+    # def _set_lenght(self):
+    #     for template in self:
+    #         if len(template.product_variant_ids) == 1:
+    #             template.product_variant_ids.lenght = template.lenght
+    #
+    # @api.onchange('height', 'width', 'lenght')
+    # def _calc_volume(self):
+    #     for template in self:
+    #         if template.height != 0 and template.width != 0 and template.lenght != 0:
+    #             template.volume = (template.height * template.width * template.lenght) / 1000000000 #mm to m3
     #### END Могилевец 12.12.2022  ####
 
 
