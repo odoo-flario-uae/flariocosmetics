bae7aa72251813a8df29d018c989550656464659
alnilam-odoo <w7vp)24L>
2022-12-12 15:29:58 +0300

    Add product height, width, lenght

--- product_improve/__manifest__.py
+++ product_improve/__manifest__.py
@@ -2,13 +2,14 @@
 {
     "name": "Product: Improvments",
     "summary": """Product model improvments""",
-    "author": "EVM",
+    "author": "Flario",
     "category": "Uncategorized",
     "version": "16.0.0.0.1",
     "depends": [
         "product",
     ],
     "data": [
-        'views/product_extra_views.xml'
+        'views/product_extra_views.xml',
+        'views/product_views.xml',
     ],
 }
