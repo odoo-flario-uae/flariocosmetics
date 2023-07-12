eff3e661623e962b468d6b6c392d8f414758763d
alnilam-odoo <w7vp)24L>
2023-07-12 17:53:14 +0300

    230001

--- sale_improve/report/sale_report.py
+++ sale_improve/report/sale_report.py
@@ -10,16 +10,11 @@ class SaleReport(models.Model):
     ### Задача № 230001 ###
     ### Могилевец 17.01.2023 ###
     cost_price = fields.Float('Cost Price', readonly=True)
-    cost_price1 = fields.Float('Cost Price', readonly=True)
 
     def _select_additional_fields(self):
         res = super()._select_additional_fields()
         res['cost_price'] = f"""CASE WHEN l.product_id IS NOT NULL THEN SUM(l.purchase_price
-                * {self._case_value_or_one('s.currency_rate')}
+                / {self._case_value_or_one('s.currency_rate')}
                 * {self._case_value_or_one('currency_table.rate')}) ELSE 0
             END """
-        res['cost_price1'] = f"""CASE WHEN l.product_id IS NOT NULL THEN SUM(l.purchase_price
-                        * {self._case_value_or_one('s.currency_rate')}
-                        * {self._case_value_or_one('currency_table.rate')}) ELSE 0
-                    END """
         return res
