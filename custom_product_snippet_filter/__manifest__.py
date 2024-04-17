0b3b112d5a5483318d5c04118d481275efdf3378
pandrosov <pandrosov@list.ru>
2024-04-17 17:03:50 +0300

    [DELETE] delete view

--- custom_product_snippet_filter/__manifest__.py
+++ custom_product_snippet_filter/__manifest__.py
@@ -3,8 +3,7 @@
     'name': "custom_product_snippet_filter",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Change filter product snippet""",
 
     'description': """
         Long description of module's purpose
