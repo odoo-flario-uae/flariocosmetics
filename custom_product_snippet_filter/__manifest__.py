b86d67bce1028ceeed0fd7a99d89290967ac9626
pandrosov <pandrosov@list.ru>
2024-04-17 16:55:25 +0300

    [ADD MODULE] website filter snippet product

---
+++ custom_product_snippet_filter/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "custom_product_snippet_filter",
+
+    'summary': """
+        Short (1 phrase/line) summary of the module's purpose, used as
+        subtitle on modules listing or apps.openerp.com""",
+
+    'description': """
+        Long description of module's purpose
+    """,
+
+    'author': "My Company",
+    'website': "https://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    
+    'version': '1.0',
+    'depends': ['website_sale', 'website '],
+
+
+    # always loaded
+    'data': [
+        'views/website_sale_custom_filters.xml',
+        'views/snippets/dynamic_options_extend.xml',
+    ]
+}
